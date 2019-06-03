6f59771ed39e72f9bab7c2460ad6763e125dfa30
ChathurangaSN <sachithmaths@gmail.com>
2019-06-03 07:59:20 +0530

    Add Home Controller and default routing

--- stock-service/src/main/java/com/sachith/controller/HomeController.java
+++ stock-service/src/main/java/com/sachith/controller/HomeController.java
@@ -4,7 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value ="/inventorycloud")
-public class StockController {
-	
+public class HomeController {
+	@RequestMapping(value ="/")
+	public String greeting() {
+		return "<h1> Wel come to Developing World..</h1>";
+	}
+
 }
